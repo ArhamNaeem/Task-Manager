a0dbdcbd5b843f05477c09ffb51e540a2beffc9f
Arham <arhamn205@gmail.com>
2023-03-04 13:44:04 +0500

    data being fetched/updated/deleted/created

--- backend/src/middleware/set_header.ts
+++ backend/src/middleware/set_header.ts
@@ -1,8 +1,12 @@
-import express from 'express';
-const setHeader = (req:express.Request, res:express.Response, next:express.NextFunction) => {
+import express from "express";
+const setHeader = (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
   res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE","PATCH");
-      res.header("Access-Control-Allow-Headers", "Content-Type");
-      next();
-}
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE,PATCH");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+};
 export default setHeader;
